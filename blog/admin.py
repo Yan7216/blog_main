745a399db603600c1e95ffd57d51b887a2213eeb
Yan7216 <aguilaryan040@gmail.com>
2026-03-18 16:00:18 -0700

    Modifique archivos de models y admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import post, category,comment
 
+admin.site.registrer(post)
+admin.site.registrer(category)
+admin.site.registrer(comment)
 # Register your models here.
